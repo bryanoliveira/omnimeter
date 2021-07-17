65ab202a8e4c12e5b024a5d66339275cf8b21b25
Bryan Lincoln <bryanufg@gmail.com>
2021-07-17 16:34:16 -0300

    feat: add extra info about cpu and gpu

--- server/plugins/cpu.py
+++ server/plugins/cpu.py
@@ -2,6 +2,7 @@
 from plugin_interface import PluginInterface
 
 # plugin imports
+import cpuinfo
 import psutil
 
 
@@ -17,7 +18,15 @@ class CPUPlugin(PluginInterface):
 
     def get_dict(self):
         freq = psutil.cpu_freq()
+        mem = psutil.virtual_memory()
         return {
+            "name": cpuinfo.get_cpu_info()["brand_raw"],
+            "utilization": psutil.cpu_percent(interval=None),
+            "memory": {
+                "max": mem.total / (1024 ** 2),
+                "current": (mem.total - mem.available) / (1024 ** 2),
+                "available": mem.available / (1024 ** 2),
+            },
             "frequency": {
                 "current": freq.current,
                 "max": freq.max,
--- server/plugins/gpu_nvidia.py
+++ server/plugins/gpu_nvidia.py
@@ -6,35 +6,29 @@ import csv
 import os
 
 
-SAFE_NAMES = {
-    "index": "index",
-    "name": "name",
-    "temperature.gpu": "temperature",
-    "utilization.gpu [%]": "utilization",
-    "utilization.memory [%]": "memory",
-    "fan.speed [%]": "fan",
-    "power.draw [W]": "power",
-    "clocks.current.sm [MHz]": "frequency_sm",
-    "clocks.current.memory [MHz]": "frequency_memory",
-    "clocks.current.graphics [MHz]": "frequency_graphics",
-    "pstate": "performance_state",
-}
-
-
 class GPUPlugin(PluginInterface):
     def __init__(self):
         # nvidia-smi --help-query-gpu
         self.cmd = (
             "nvidia-smi -i 0 --query-gpu=index,name,"
             + "temperature.gpu,"
+            + "temperature.memory,"
+            + "encoder.stats.averageFps"
             + "utilization.gpu,"
             + "utilization.memory,"
+            + "memory.total,"
+            + "memory.used,"
+            + "memory.free,"
             + "fan.speed,"
             + "power.draw,"
-            + "clocks.sm,"
+            + "power.limit,"
+            + "clocks.current.sm,"
+            + "clocks.max.sm,"
+            + "clocks.current.graphics,"
+            + "clocks.max.graphics,"
+            + "clocks.current.memory,"
+            + "clocks.max.memory,"
             + "pstate,"
-            + "clocks.mem,"
-            + "clocks.gr"
             + " --format=csv,nounits -l1"
         )
         print("GPU command:", self.cmd)
@@ -52,7 +46,23 @@ class GPUPlugin(PluginInterface):
         data = self.get_gpu_data()
         gpus_info = {}
         for info in data:
-            gpus_info[int(info["index"])] = info
+            gpus_info[int(info["index"])] = {
+                "name": info["name"],
+                "utilization": info["utilization.gpu"],
+                "memory": {
+                    "max": info["memory.total"],
+                    "current": info["memory.used"],
+                    "available": info["memory.free"],
+                },
+                "power": {
+                    "max": info["power.limit"],
+                    "current": info["power.draw"],
+                },
+                "frequency": {
+                    "current": info["clocks.current.sm"],
+                    "max": info["clocks.max.sm"],
+                },
+            }
         return gpus_info
 
     def get_gpu_data(self):
@@ -63,7 +73,7 @@ class GPUPlugin(PluginInterface):
         for row in rows[1:]:
             data.append(
                 {
-                    SAFE_NAMES[key.strip()]: value.strip()
+                    key.strip().split(" ")[0]: value.strip()
                     for key, value in zip(headers, row)
                 }
             )
