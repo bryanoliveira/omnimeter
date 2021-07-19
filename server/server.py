70df568538eed8fbea8ae77f4df206717b4e7a41
Bryan L. M. de Oliveira <bryanufg@gmail.com>
2021-07-19 00:29:35 -0300

    feat: add app running via main

--- server/server.py
+++ server/server.py
@@ -5,35 +5,8 @@ import pkgutil
 from flask import Flask
 from plugin_interface import PluginInterface
 
-
 app = Flask(__name__)
-
-
 plugins = []
-try:
-    for loader, modname, _ in pkgutil.walk_packages(path=["./plugins"]):
-        print("Found module", modname)
-        module = loader.find_module(modname).load_module(modname)
-        print("Imported module", modname)
-        for class_name, class_type in inspect.getmembers(module, inspect.isclass):
-            print("Found class", class_name, class_type)
-            if class_name != "PluginInterface" and issubclass(
-                class_type, PluginInterface
-            ):
-                print("Found plugin", class_type)
-                plugins.append(class_type())
-                print("Installed plugin", class_type)
-except ImportError as e:
-    print(e)
-except Exception as e:
-    print(e)
-
-print("Plugins:", plugins)
-
-print("Waking up device")
-os.popen("adb shell input keyevent KEYCODE_WAKEUP")
-print("Starting server...")
-
 
 @app.route("/")
 def get_stats():
@@ -61,3 +34,29 @@ def get_plugins():
         }
         for plugin in plugins
     }
+
+if __name__ == "__main__":
+    try:
+        for loader, modname, _ in pkgutil.walk_packages(path=["./plugins"]):
+            print("Found module", modname)
+            module = loader.find_module(modname).load_module(modname)
+            print("Imported module", modname)
+            for class_name, class_type in inspect.getmembers(module, inspect.isclass):
+                print("Found class", class_name, class_type)
+                if class_name != "PluginInterface" and issubclass(
+                    class_type, PluginInterface
+                ):
+                    print("Found plugin", class_type)
+                    plugins.append(class_type())
+                    print("Installed plugin", class_type)
+    except ImportError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+
+    print("Plugins:", plugins)
+
+    print("Waking up device")
+    os.popen("adb shell input keyevent KEYCODE_WAKEUP")
+    print("Starting server...")
+    app.run(host="0.0.0.0", port="5000", use_reloader=True, use_debugger=True)
