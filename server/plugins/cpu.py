e6fe94a19247208a443ff11fcebaeb075c2fe3af
Bryan L. M. de Oliveira <bryanufg@gmail.com>
2021-07-21 00:00:08 -0300

    feat: add temperature support for windows
    Using OpenHardwareMonitorLib

--- server/plugins/cpu.py
+++ server/plugins/cpu.py
@@ -7,6 +7,50 @@ import cpuinfo
 import psutil
 
 
+### WINDOWS
+if os.name == 'nt':
+    import wmi
+    import clr #package pythonnet, not clr
+
+    handle = None
+
+    clr.AddReference(os.path.abspath('./lib/windows/OpenHardwareMonitorLib.dll'))
+    from OpenHardwareMonitor import Hardware
+
+    handle = Hardware.Computer()
+    handle.CPUEnabled = True
+    # handle.MainboardEnabled = True
+    # handle.RAMEnabled = True
+    # handle.GPUEnabled = True
+    # handle.HDDEnabled = True
+    handle.Open()
+
+    def fetch_stats():
+        global handle
+        for i in handle.Hardware:
+            i.Update()
+            for sensor in i.Sensors:
+                print("sensors", (sensor.Index, sensor.Hardware.HardwareType, sensor.SensorType, sensor.Hardware.Name, sensor.Name, sensor.Value))
+            for j in i.SubHardware:
+                j.Update()
+                for subsensor in j.Sensors:
+                    print("subsensor", (subsensor.Index, subsensor.Hardware.HardwareType, subsensor.SensorType, subsensor.Hardware.Name, subsensor.Name, subsensor.Value))
+
+    def get_cpu_temperature() -> float:
+        global handle
+        if handle is None: return
+        for i in handle.Hardware:
+            i.Update()
+            for sensor in i.Sensors:
+                if "CPU Package" in sensor.Name and sensor.SensorType == 2: # 2 = Temperature
+                    return float(sensor.Value)
+        return 0.0
+
+### POSIX
+else:
+    def get_cpu_temperature() -> float:
+        psutil.sensors_temperatures()["k10temp"][0].current
+
 class CPUPlugin(PluginInterface):
     def get_id(self):
         return "default_cpu"
@@ -20,6 +64,7 @@ class CPUPlugin(PluginInterface):
     def get_dict(self):
         freq = psutil.cpu_freq()
         mem = psutil.virtual_memory()
+        
         return {
             "name": " ".join(cpuinfo.get_cpu_info()["brand_raw"].split(" ")[:4]),
             "utilization": psutil.cpu_percent(interval=None),
@@ -33,5 +78,5 @@ class CPUPlugin(PluginInterface):
                 "max": freq.max,
                 "min": freq.min,
             },
-            "temperature": psutil.sensors_temperatures()["k10temp"][0].current if os.name == "posix" else 0.0,
+            "temperature": get_cpu_temperature(),
         }
