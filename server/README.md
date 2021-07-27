37c02943faf19efbe98b25791a04d04fef53c5fb
Bryan Lincoln <bryanufg@gmail.com>
2021-07-27 20:06:50 -0300

    feat: add service & shortcuts & install script

--- server/README.md
+++ server/README.md
@@ -1,3 +1,9 @@
 Requires Python 3.7 because of pythonnet.
 Select Python betwen 32 and 64-bit according to your system (it will not work otherwise - os.popen will fail).
 On Windows it should run as admin to be able to read CPU temperature sensors (OpenHardwareMonitorLib).
+
+Steps:
+
+- Create a python env with requirements.txt (and requirements-windows.txt if running on Windows)
+- `cd` into `omnimeter/` and run `python main.py` to check if it works
+- Run `bash install.sh`
---
+++ server/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DIRNAME=$(dirname $(realpath "$0"))
+
+# create .desktop and .service files for current installation
+cp omnimeter.service.example omnimeter.service
+sed -i "s+<OMNIMETERFOLDER>+$DIRNAME+" omnimeter.service
+cp omnimeter.desktop.example omnimeter.desktop
+sed -i "s+<OMNIMETERFOLDER>+$DIRNAME+" omnimeter.desktop
+cp run.sh.example run.sh
+sed -i "s+<PYTHONEXECUTABLE>+$(which python)+" run.sh
+sed -i "s+<OMNIMETERFOLDER>+$DIRNAME+" run.sh
+
+# get necessary environment variables
+printenv > sysenv
+
+# install icon
+icons_folder=~/.icons
+mkdir -p $icons_folder
+rm -rf $icons_folder/omnimeter.svg
+cp ./omnimeter/icon.svg $icons_folder/omnimeter.svg
+echo "Installed Icon in" $icons_folder
+
+# install desktop shortcut
+shortcuts_folder=~/.local/share/applications/
+mkdir -p $shortcuts_folder
+rm -rf $shortcuts_folder/omnimeter.desktop
+cp ./omnimeter.desktop $shortcuts_folder/omnimeter.desktop
+echo "Installed Desktop shortcut in" $shortcuts_folder
+
+services_folder=/etc/systemd/system
+sudo cp ./omnimeter.service $services_folder/omnimeter.service
+sudo systemctl daemon-reload
+sudo systemctl enable omnimeter
+sudo systemctl start omnimeter
+echo "Installed Service in" $services_folder
