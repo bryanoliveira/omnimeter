cadaea760212a8c6fac9201c9d631217ad4b44cf
Bryan L M Oliveira <bryanlmoliveira@gmail.com>
2026-02-21 09:10:52 -0300

    update for windows

---
+++ server/omnimeter/lib/windows/python3.7.9/Lib/site-packages/PySimpleGUIQt/__init__.py
@@ -0,0 +1,2 @@
+name = "PySimpleGUIQt"
+from .PySimpleGUIQt import *
---
+++ server/omnimeter/lib/windows/python3.7.9/Scripts/cpuinfo-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\bryan\OneDrive\Documentos\omnimeter\python3.7.9\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'py-cpuinfo==8.0.0','console_scripts','cpuinfo'
+__requires__ = 'py-cpuinfo==8.0.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('py-cpuinfo==8.0.0', 'console_scripts', 'cpuinfo')()
+    )
